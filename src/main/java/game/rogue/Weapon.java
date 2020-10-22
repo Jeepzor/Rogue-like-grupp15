f3eacc77b5ead878046253ef9e7c104e91c5810b
Johannesgyr <johannesgyr@hotmail.com>
2020-10-22 12:39:25 +0200

    Added more tests to WeaponTest
    Implemented functionality to match

--- src/main/java/game/rogue/Weapon.java
+++ src/main/java/game/rogue/Weapon.java
@@ -3,13 +3,19 @@ package game.rogue;
 public class Weapon extends Item{
     private static final int DEFAULT_WEIGHT = 800;
     private int damage;
+    private boolean twoHanded;
 
-    public Weapon (int damage){
+    public Weapon (int damage, boolean twoHanded){
         super(DEFAULT_WEIGHT);
         this.damage = damage;
+        this.twoHanded = twoHanded;
     }
+
 
     public int getDamage(){
         return this.damage;
+    }
+    public boolean isTwoHanded(){
+        return this.twoHanded;
     }
 }
--- src/test/java/game/rogue/WeaponTest.java
+++ src/test/java/game/rogue/WeaponTest.java
@@ -7,10 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class WeaponTest {
 
     @Test
-    public void weaponDamageTest(){
-        Weapon weapon = new Weapon(5);
+    public void weaponDefaultWeightDamageTest(){
+        Weapon weapon = new Weapon(5, false);
         assertEquals(5, weapon.getDamage());
         assertEquals(800, weapon.getWeight());
+        assertEquals(false, weapon.isTwoHanded());
     }
 
 }
