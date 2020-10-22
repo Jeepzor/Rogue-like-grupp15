e168781ffb5e0734c86d5384da348b3f0985bd3d
Johannesgyr <johannesgyr@hotmail.com>
2020-10-22 17:39:12 +0200

    Updated consume() method
    Several items will implement this differently.

--- src/main/java/game/rogue/Consumable.java
+++ src/main/java/game/rogue/Consumable.java
@@ -2,11 +2,12 @@ package game.rogue;
 
 public abstract class Consumable extends Item{
     private int size;
+
     public Consumable(int weight, int size){
         super(weight);
         this.size = size;
     }
-    public abstract void consume(Consumable item);
+    public abstract void consume(Player player);
 
     public int getSize(){
         return this.size;
